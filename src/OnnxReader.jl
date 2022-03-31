198f396f1722bc5b71239d7e0127686fa42d57a6
philipp <philipp.kern@kit.edu>
2022-03-31 16:51:25 +0200

    Convert PyObject types to Julia types

--- src/OnnxReader.jl
+++ src/OnnxReader.jl
@@ -12,10 +12,14 @@ function __init__()
 end
 
 # just interface to onnx_reader.py 
-function load_network(filename)
+function load_network(filename; dtype=AbstractFloat)
     or = onnx_reader.OnnxReader(filename)
     or.load_onnx_network()
     ws, bs = or.get_weights_and_biases()
+
+    ws = [convert.(Vector{dtype}, w) for w in ws]  # w is now vec of vecs
+    ws = [convert(Matrix, reduce(hcat, w)') for w in ws]  # w is now matrix
+    bs = [convert.(dtype, b) for b in bs]
 
     return ws, bs
 end
